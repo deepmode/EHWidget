57731fb014fc3a642da1654392892fde72df4ed5
Eric Ho <ericho@101medialabs-imac-ho-ho.local>
2017-01-27 15:58:29 +0800

    WIP

--- HBWidget/TodayViewController.swift
+++ HBWidget/TodayViewController.swift
@@ -10,49 +10,52 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDelegate, UITableViewDataSource {
-
     
     @IBOutlet weak var tableView:UITableView!
-    private var dataSrc:[HBChannel] = [HBChannel]() {
+    private var dataSrc:[HBWidgetPost] = [HBWidgetPost]() {
         didSet {
             self.tableView.reloadData()
         }
     }
     
-    //private let rowHeight:CGFloat = 100.0
-        
+    static let maxSize = 20
+    
+//    func setupDataSrc() {
+//        print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
+//        var tempChannels = [HBChannel]()
+//        for eachIndex in 0...TodayViewController.maxSize {
+//            let hbc = HBChannel(channelIdentifier: "identifier \(eachIndex)", channelDisplayName: "Channel Display name Channel Display Name Channel Display name Channel Display Name Channel Display Name Channel Display Name \(eachIndex)", channelDescription: "\(eachIndex) : Channel Description. Plastic has become a vital part of our lives of convenience.", enableChannel: true, enableNotification: true)
+//            tempChannels.append(hbc)
+//        }
+//        self.dataSrc = tempChannels
+//    }
+    
+    let estimateTableViewCellHeight:CGFloat = 120.0
     override func viewDidLoad() {
         print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
         
+        self.tableView.register(UINib(nibName: "HBNewsWidgetCell", bundle: Bundle.main), forCellReuseIdentifier: "Cell_NewsWidget")
+        
+        //------------------
+        // setup tableView to support dynamic tableview cell height
+        self.tableView.estimatedRowHeight = self.estimateTableViewCellHeight
+        self.tableView.rowHeight = UITableViewAutomaticDimension;
+        //------------------
+        
         self.tableView.delegate = self
         self.tableView.dataSource = self
         
-        self.setupDataSrc()
+        //self.setupDataSrc()
+        
+        self.tableView.reloadData()
+        self.updatePreferredContentSize()
+        
+        self.view.backgroundColor = UIColor.green
         
     }
     
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
-        super.viewWillAppear(animated)
-        self.tableView.reloadData()
-        self.updatePreferredContentSize()
-    }
-    
-    static let maxSize = 10
-    
-    func setupDataSrc() {
-        print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
-        var tempChannels = [HBChannel]()
-        for eachIndex in 0...TodayViewController.maxSize {
-            let hbc = HBChannel(channelIdentifier: "identifier \(eachIndex)", channelDisplayName: "Channel Display name \(eachIndex)", channelDescription: "\(eachIndex) : Channel Description. Plastic has become a vital part of our lives of convenience.", enableChannel: true, enableNotification: true)
-            tempChannels.append(hbc)
-        }
-        self.dataSrc = tempChannels
-    }
     
     override func didReceiveMemoryWarning() {
         print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
@@ -68,9 +71,13 @@ class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDeleg
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
         
-        self.tableView.reloadData()
-        self.updatePreferredContentSize()
-        completionHandler(NCUpdateResult.newData)
+        HBApi.getNewsFeed("https://hypebeast.com/") { (request, response, widgetPosts, st, error) in
+            
+            self.dataSrc = widgetPosts!
+            self.tableView.reloadData()
+            self.updatePreferredContentSize()
+            completionHandler(NCUpdateResult.newData)
+        }
     }
     
     
@@ -78,10 +85,11 @@ class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDeleg
     
     func updatePreferredContentSize() {
         print("----- \(NSStringFromClass(self.classForCoder)).\(#function) -----")
-        let rHeight:CGFloat = 20.0
+        let rHeight:CGFloat = self.estimateTableViewCellHeight
         var newSize = self.preferredContentSize
         //newSize.height = CGFloat(self.tableView.numberOfRows(inSection: 0)) * rHeight //CGFloat(self.tableView.rowHeight)
-        newSize.height = CGFloat(self.tableView.numberOfRows(inSection: 0)) * rHeight
+        //newSize.height = CGFloat(self.tableView.numberOfRows(inSection: 0)) * rHeight
+        newSize.height = CGFloat(3.0) * rHeight
         self.preferredContentSize = newSize
     }
     
@@ -98,16 +106,14 @@ class TodayViewController: UIViewController, NCWidgetProviding, UITableViewDeleg
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell_Identifier", for: indexPath as IndexPath)
-        let hbc = self.dataSrc[indexPath.row]
-            
-        cell.selectionStyle =  UITableViewCellSelectionStyle.default //UITableViewCellSelectionStyle.None
-        cell.layoutMargins =  UIEdgeInsets.zero
-        cell.textLabel?.text = hbc.channelDisplayName
-        cell.textLabel?.textColor = UIColor.lightText
-        cell.detailTextLabel?.text = hbc.channelDescription
-        cell.detailTextLabel?.textColor = UIColor.lightText
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell_NewsWidget", for: indexPath as IndexPath) as! HBNewsWidgetCell
+        
+        let post = self.dataSrc[indexPath.row]
+        cell.setupCellWith(post: post)
+        
+        cell.selectionStyle =  UITableViewCellSelectionStyle.none //if set to default, iOS 9 - after selected, won't diselect
+        cell.layoutMargins =  UIEdgeInsets.zero
         
         //----- SUPER IMPORTANT -----
         //Fix the left alignment issue for custom cell to sync with the default UITableCell's behaviour
---
+++ Podfile
@@ -0,0 +1,21 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'HBWidget' do
+    
+
+    
+    #pod 'Alamofire', '~> 3.0'
+    #For xcode 7.x, switf 2.3 & ios 8 or above
+    #pod 'Alamofire', :git => 'https://github.com/Alamofire/Alamofire.git', :tag => '3.5.0'
+    pod 'Alamofire'
+    
+    #pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
+    #pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git', :tag => '2.4.0'
+    pod 'SwiftyJSON'
+    
+    pod 'SDWebImage'
+
+    
+end
